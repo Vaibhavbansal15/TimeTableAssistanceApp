6e46b93a8b8229e3d30aeeb0e14db41c291eef50
Vaibhavbansal15 <vaibhavbansal1501@gmail.com>
2023-11-09 20:05:00 +0530

    Developed reset password feature

--- TimeTableAssistance/app/src/main/java/com/minorproject/timetableassistance/ResetPassword.kt
+++ TimeTableAssistance/app/src/main/java/com/minorproject/timetableassistance/ResetPassword.kt
@@ -2,10 +2,33 @@ package com.minorproject.timetableassistance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class ResetPassword : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reset_password)
+
+        auth = FirebaseAuth.getInstance()
+
+        val resetBtn = findViewById<Button>(R.id.resetPasswordBtn)
+
+        resetBtn.setOnClickListener{
+            val resetEmail : String = findViewById<EditText>(R.id.resetPasswordEmail).text.toString()
+            auth.sendPasswordResetEmail(resetEmail)
+                .addOnCompleteListener(this) { task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(this, "Check your email.", Toast.LENGTH_SHORT).show()
+                    } else{
+                        Toast.makeText(this, "Failed : ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
     }
 }
